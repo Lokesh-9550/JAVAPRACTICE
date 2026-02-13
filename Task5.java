f69d51d0c0526cd99fe62bf1727bc5f2d45f03ab
Lokesh <puttamlokesh9550@gmail.com>
2026-02-13 10:41:10 +0530

    Task5

---
+++ Task5.java
@@ -0,0 +1,31 @@
+class  Demo{
+    int num1,num2;
+    Demo(){
+        super();
+        System.out.println("Zero param constructor.");
+    }
+    Demo(int num1,int num2){
+        this.num1=num1;
+        this.num2=num2;
+        System.out.println("two param constructor.");
+    }
+    Demo(int num1){
+        this();
+        this.num1=num1;
+        num2=44;
+        System.out.println("one param constructor.");
+    }
+   void display(){
+        System.out.println("num1: "+num1);
+        System.out.println("num2: "+num2);
+    }
+}
+
+public class Task5 {
+    public static void main(String[] args) {
+        Demo demo=new Demo(21);
+        demo.display();
+
+    }
+    
+}
