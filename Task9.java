4c598c78d602dc58331251381341dab136c2d1ae
Lokesh <puttamlokesh9550@gmail.com>
2026-02-14 12:11:06 +0530

    Task9

---
+++ Task9.java
@@ -0,0 +1,19 @@
+class Alien{
+    
+    static{
+        display();
+    }
+    static int a=10;
+    static void display(){
+        System.out.println("a: "+a);
+    }
+
+}
+
+public class Task9 {
+    public static void main(String[] args) {
+        Alien a=new Alien();
+        
+    }
+    
+}
